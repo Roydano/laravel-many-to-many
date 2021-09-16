4d118c6cfcdbe4549fe5808b85ebd19276649560
Roydano <roy.dano90@gmail.com>
2021-09-16 14:20:14 +0200

    init

---
+++ app/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable =[
+
+        'title',
+        'slug',
+        'description',
+        'author',
+        'category_id'
+    ];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+}
+
